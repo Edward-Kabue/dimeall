9b851415093a4dfea9e5df53728674f4aa8bb14f
Baricho <ekabue0@gmail.com>
2023-01-11 13:41:41 +0300

    config

--- dimeFront/next.config.js
+++ dimeFront/next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
+const path = require('path');
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';  
+`;
+const securityHeaders = [ContentSecurityPolicy];
 
 const nextConfig = {
   reactStrictMode: false,
   sassOptions: {
-    includePaths: [path.join(__dirname, "css")],
+    includePaths: [path.join(__dirname, 'css')],
   },
   trailingSlash: true,
   devIndicators: {
@@ -13,6 +22,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
-}
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
