ed4c8533b5b1e3cda81c0dd112671cb05676dbd7
Baricho <ekabue0@gmail.com>
2023-01-11 15:51:36 +0300

    fe

--- dimeFront/next.config.js
+++ dimeFront/next.config.js
@@ -13,35 +13,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/graphql',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
-  rewrites: {
-    return: [
-      {
-        source: 'https://cms.dimetechgroup.com/graphql',
-        destination: 'https://cms.dimetechgroup.com/graphql',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
