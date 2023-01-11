33a8dcd97009428ac984005196ded3f54a0fc5d3
Baricho <ekabue0@gmail.com>
2023-01-11 15:42:01 +0300

    gh

--- dimeFront/next.config.js
+++ dimeFront/next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: 'https://cms.dimetechgroup.com/graphql',
+        source: '/graphql/*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
